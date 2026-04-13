c4ff5cb7833958363119fb9aff4cf4b58d5cad6b
Nikhil <nikhilraikwar099@gmail.com>
2026-04-14 00:53:07 +0530

    backend url changed

--- frontend/public/assets.js
+++ frontend/public/assets.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = "https://remix-andale-reynolds-completing.trycloudflare.com";
+const baseURL = "https://holders-demographic-approval-suited.trycloudflare.com";
 export const backendUrl = axios.create({ baseURL });
 backendUrl.interceptors.request.use((cfg) => {
   const token = localStorage.getItem("token");
