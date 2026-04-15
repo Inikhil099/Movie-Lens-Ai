886bb577e0d6d4da47b0384cca0905ac701d632c
Nikhil <nikhilraikwar099@gmail.com>
2026-04-15 15:34:10 +0530

    deployed

--- frontend/public/assets.js
+++ frontend/public/assets.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-const baseURL = "https://holders-demographic-approval-suited.trycloudflare.com";
+const url = process.env.NEXT_PUBLIC_API_URL;
+const baseURL = url;
 export const backendUrl = axios.create({ baseURL });
 backendUrl.interceptors.request.use((cfg) => {
   const token = localStorage.getItem("token");
   if (token) cfg.headers.Authorization = `Bearer ${token}`;
   return cfg;
 });
-// export const backendUrl = "https://movie-lens-ai.onrender.com";
